57a6eaf0c21f5fe3addcbb541424db1241b1e9ba
fanhl <dyguests@qq.com>
2024-02-28 16:53:50 +0800

    登录逻辑

--- app/src/main/java/com/fanhl/helloamazon/MainActivity.kt
+++ app/src/main/java/com/fanhl/helloamazon/MainActivity.kt
@@ -1,16 +1,63 @@
 package com.fanhl.helloamazon
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.amazon.identity.auth.device.AuthError
+import com.amazon.identity.auth.device.api.authorization.AuthCancellation
+import com.amazon.identity.auth.device.api.authorization.AuthorizationManager
+import com.amazon.identity.auth.device.api.authorization.AuthorizeListener
+import com.amazon.identity.auth.device.api.authorization.AuthorizeRequest
+import com.amazon.identity.auth.device.api.authorization.AuthorizeResult
+import com.amazon.identity.auth.device.api.authorization.ProfileScope
+import com.amazon.identity.auth.device.api.workflow.RequestContext
+
 
 class MainActivity : AppCompatActivity() {
+    private var requestContext: RequestContext? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestContext = RequestContext.create(this);
+
+        requestContext?.registerListener(object : AuthorizeListener() {
+            /*授权已成功完成。*/
+            override fun onSuccess(result: AuthorizeResult) {
+                /*您的应用现已获得请求范围授权*/
+                Log.d(TAG, "您的应用现已获得请求范围授权 accessToken:${result.accessToken}")
+            }
+
+            /*尝试授权应用时出错。*/
+            override fun onError(ae: AuthError) {
+                /*提示用户发生错误*/
+                Log.d(TAG, "提示用户发生错误")
+            }
+
+            /*授权未完成便已取消。*/
+            override fun onCancel(cancellation: AuthCancellation) {
+                /*将UI重新设置为随时登录状态*/
+                Log.d(TAG, "将UI重新设置为随时登录状态")
+            }
+        })
+
         findViewById<Button>(R.id.btn_amazon).setOnClickListener {
-            //
+            AuthorizationManager.authorize(
+                AuthorizeRequest.Builder(requestContext)
+                    .addScopes(ProfileScope.profile(), ProfileScope.postalCode())
+                    .build()
+            )
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requestContext?.onResume()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
