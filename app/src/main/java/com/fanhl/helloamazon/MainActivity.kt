5b5723998fdd4b9a21a6c989080a64be2a1a40dc
fanhl <dyguests@qq.com>
2024-02-28 16:59:20 +0800

    登录测试代码

--- app/src/main/java/com/fanhl/helloamazon/MainActivity.kt
+++ app/src/main/java/com/fanhl/helloamazon/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fanhl.helloamazon
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.amazon.identity.auth.device.AuthError
 import com.amazon.identity.auth.device.api.authorization.AuthCancellation
@@ -15,34 +16,13 @@ import com.amazon.identity.auth.device.api.workflow.RequestContext
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
     private var requestContext: RequestContext? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        requestContext = RequestContext.create(this);
-
-        requestContext?.registerListener(object : AuthorizeListener() {
-            /*授权已成功完成。*/
-            override fun onSuccess(result: AuthorizeResult) {
-                /*您的应用现已获得请求范围授权*/
-                Log.d(TAG, "您的应用现已获得请求范围授权 accessToken:${result.accessToken}")
-            }
-
-            /*尝试授权应用时出错。*/
-            override fun onError(ae: AuthError) {
-                /*提示用户发生错误*/
-                Log.d(TAG, "提示用户发生错误")
-            }
-
-            /*授权未完成便已取消。*/
-            override fun onCancel(cancellation: AuthCancellation) {
-                /*将UI重新设置为随时登录状态*/
-                Log.d(TAG, "将UI重新设置为随时登录状态")
-            }
-        })
-
+        textView = findViewById<TextView>(R.id.textView)
         findViewById<Button>(R.id.btn_amazon).setOnClickListener {
             AuthorizationManager.authorize(
                 AuthorizeRequest.Builder(requestContext)
@@ -50,6 +30,33 @@ class MainActivity : AppCompatActivity() {
                     .build()
             )
         }
+
+        requestContext = RequestContext.create(this);
+        requestContext?.registerListener(object : AuthorizeListener() {
+            /*授权已成功完成。*/
+            override fun onSuccess(result: AuthorizeResult) {
+                /*您的应用现已获得请求范围授权*/
+                val msg = "您的应用现已获得请求范围授权 accessToken:${result.accessToken}"
+                Log.d(TAG, msg)
+                textView.text = msg
+            }
+
+            /*尝试授权应用时出错。*/
+            override fun onError(ae: AuthError) {
+                /*提示用户发生错误*/
+                val msg = "提示用户发生错误"
+                Log.d(TAG, msg)
+                textView.text = msg
+            }
+
+            /*授权未完成便已取消。*/
+            override fun onCancel(cancellation: AuthCancellation) {
+                /*将UI重新设置为随时登录状态*/
+                val msg = "将UI重新设置为随时登录状态"
+                Log.d(TAG, msg)
+                textView.text = msg
+            }
+        })
     }
 
     override fun onResume() {
